6d2bfe115cc5882351c2e2ae445c731397065bce
Lauren Zygarewicz <lzygarew@berkeley.edu>
2023-03-30 17:22:54 -0700

    Compartmentalized random board functionality, store sudoku boards

--- random_board.py
+++ random_board.py
@@ -7,6 +7,12 @@ import requests as reqs
 import random
 
 BOARD_LEN = 9
+NUM_LEVELS = 4
+MASTER_ARR = []
+LINK_ARR = ["https://raw.githubusercontent.com/grantm/sudoku-exchange-puzzle-bank/master/easy.txt",
+            "https://raw.githubusercontent.com/grantm/sudoku-exchange-puzzle-bank/master/medium.txt",
+            "https://raw.githubusercontent.com/grantm/sudoku-exchange-puzzle-bank/master/hard.txt",
+            "https://raw.githubusercontent.com/grantm/sudoku-exchange-puzzle-bank/master/diabolical.txt"]
 
 def engine():
     level = int(input("Enter level of difficulty: \n(Easy = 1 | Medium = 2 | Hard = 3 | Diabolical = 4)\n"))
@@ -16,26 +22,35 @@ def engine():
             level = input("Invalid input. Please enter a number from 1-4 corresponding to difficulty level\n1 = easiest, 4 = hardest\n")
             correct_input = level == 1 or level == 2 or level == 3 or level == 4
     
+    # get a random line (sudoku board) from the link corresponding to the requested difficulty level
+    puzzle_line = get_board(level)
+    # parse through line to get board
+    board = parse_board(puzzle_line)
+    fill_board(board)
+
+def get_board(level):
     link = ""
-    if level == 1:
-        link = "https://raw.githubusercontent.com/grantm/sudoku-exchange-puzzle-bank/master/easy.txt"
-    elif level == 2:
-        link = "https://raw.githubusercontent.com/grantm/sudoku-exchange-puzzle-bank/master/medium.txt"
-    elif level == 3:
-        link = "https://raw.githubusercontent.com/grantm/sudoku-exchange-puzzle-bank/master/hard.txt"
-    else:
-        link = "https://raw.githubusercontent.com/grantm/sudoku-exchange-puzzle-bank/master/diabolical.txt"
-    
+    if MASTER_ARR == []:
+        for i in range(NUM_LEVELS):
+            MASTER_ARR.append([])
+
+    # zero-indexing
+    level -= 1
+
+    if MASTER_ARR[level] == []:
+        link = LINK_ARR[level]
+        MASTER_ARR[level] = retrieve_arr(link)
+    # pick random line (sudoku) to retrieve
+    puzzle_num = random.randint(0, len(MASTER_ARR[level]) - 1)
+    puzzle_line = (MASTER_ARR[level])[puzzle_num]
+    return puzzle_line
+
+def retrieve_arr(link):
     page = reqs.get(link)
     arr = []
     for line in page.text.split('\n'):
         arr += [''.join(line)]
-    # pick random line (sudoku) to retrieve
-    puzzle_num = random.randint(0, len(arr) - 1)
-    puzzle_line = arr[puzzle_num]
-    # parse through line to get board
-    board = parse_board(puzzle_line)
-    fill_board(board)
+    return arr
 
 def parse_board(line):
     board_line = ""
